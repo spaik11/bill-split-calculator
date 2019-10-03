d9aae9bfc19643b7e6656100245e3810ea7eb08e
Sung Paik <sung.paik@codeimmersives.com>
2019-10-03 14:24:43 -0400

    had to convert user input to numbers

--- bill-split-calc.js
+++ bill-split-calc.js
@@ -1,17 +1,18 @@
-let bill = process.argv[2]
-let people = process.argv[3]
-let tipPercent = process.argv[4]
+let bill = Number(process.argv[2])
+let people = Number(process.argv[3])
+let tipPercent = Number(process.argv[4])
 
 // let bill = 100;
 // let people = 2;
-// let tipPercent = '';
+// let tipPercent = 40;
 
 if (tipPercent > 0) {
-    const totalWithTip = bill + (tipPercent * bill) / 100;
-    const total = totalWithTip / people;
+    const withTip = bill + tipPercent * bill / 100;
+    const total = withTip / people;
     console.log(total);
 } else {
     const total = bill / people;
     console.log(total);
 }
 
+Number()
