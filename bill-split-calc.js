690ca9143a22316a8289fe14429380acb3d40e0a
Sung Paik <sung.paik@codeimmersives.com>
2019-10-03 15:05:16 -0400

    cleaned up ui

--- bill-split-calc.js
+++ bill-split-calc.js
@@ -2,11 +2,11 @@ const bill = Number(process.argv[2])
 const people = Number(process.argv[3])
 const tipPercent = Number(process.argv[4])
 
-console.log(`$${bill.toFixed(2)} - Bill`);
+console.log(`The Bill - $${bill.toFixed(2)}`);
 
 function theTip(bill, tipPercent) {
     result = tipPercent * bill / 100;
-    console.log(`$${result.toFixed(2)} - The Tip`);
+    console.log(`The Tip - $${result.toFixed(2)}`);
 }
 
 theTip(bill, tipPercent);
@@ -14,10 +14,10 @@ theTip(bill, tipPercent);
 if (tipPercent > 0) {
     const withTip = bill + tipPercent * bill / 100;
     const total = withTip / people;
-    console.log(`$${total.toFixed(2)} each. Thanks for the tip!`);
+    console.log(`$${total.toFixed(2)} per person. Thanks for the tip!`);
 } else {
     const total = bill / people;
-    console.log(`$${total.toFixed(2)} each. See you next time!`);
+    console.log(`$${total.toFixed(2)} per person. See you next time!`);
 }
 
 
