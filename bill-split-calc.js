ab10807bc70c8d3803c971ac642091fa1f31a36b
Sung Paik <sung.paik@codeimmersives.com>
2019-10-03 15:57:59 -0400

    including user experience more dynamic

--- bill-split-calc.js
+++ bill-split-calc.js
@@ -1,11 +1,16 @@
 const bill = Number(process.argv[2])
 const people = Number(process.argv[3])
-const tipPercent = Number(process.argv[4])
+const tip = Number(process.argv[4])
+
+// const result = process.argv[2].startsWith('tip')
+// result
+// const tip = process.argv[2].slice(-2)
+// tip
 
 console.log(`The Bill - $${bill.toFixed(2)}`);
 
-function theTip(bill, tipPercent) {
-    result = tipPercent * bill / 100;
+function theTip(bill, tip) {
+    result = tip * bill / 100;
     if (result > 0) {
     console.log(`The Tip - $${result.toFixed(2)}`);
 } else {
@@ -13,10 +18,10 @@ function theTip(bill, tipPercent) {
 }
 }
 
-theTip(bill, tipPercent);
+theTip(bill, tip);
 
-if (tipPercent > 0) {
-    const withTip = bill + tipPercent * bill / 100;
+if (tip > 0) {
+    const withTip = bill + tip * bill / 100;
     const total = withTip / people;
     console.log(`$${total.toFixed(2)} per person. Thanks for the tip!`);
 } else {
