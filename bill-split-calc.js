517d02a0a9ae6de6e582fdc7b42ad4c8c3966c8b
Sung Paik <sung.paik@codeimmersives.com>
2019-10-03 14:06:56 -0400

    app works with set variables but not with node input

--- bill-split-calc.js
+++ bill-split-calc.js
@@ -1,4 +1,17 @@
-const theBill = process.argv[2]
-const peopleToSplitWith = process.argv[3]
-const ifYouWantToTip = process.argv [4]
+let bill = process.argv[2]
+let people = process.argv[3]
+let tipPercent = process.argv[4]
+
+// let bill = 100;
+// let people = 2;
+// let tipPercent = '';
+
+if (tipPercent > 0) {
+    const totalWithTip = bill + (tipPercent * bill) / 100;
+    const total = totalWithTip / people;
+    console.log(total);
+} else {
+    const total = bill / people;
+    console.log(total);
+}
 
